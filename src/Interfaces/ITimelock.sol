ad3e1d97abc9fd32f2fd822fa13916acc93f3b07
omoboi_dev <bukunmiabiodun14@gmail.com>
2026-03-11 03:49:28 +0100

    feat: Updated my Interfaces

--- src/Interfaces/ITimelock.sol
+++ src/Interfaces/ITimelock.sol
@@ -2,13 +2,18 @@
 pragma solidity ^0.8.20;
 
 interface ITimelock {
-
     event ProposalQueued(bytes32 indexed proposalId, uint256 unlockTime);
+
     event ProposalDischarged(bytes32 indexed proposalId);
 
     function enqueue(bytes32 proposalId) external;
 
-    function discharge(bytes32 proposalId) external;
+    function discharge(
+        bytes32 proposalId,
+        address target,
+        uint256 value,
+        bytes calldata callData
+    ) external;
 
     function isReady(bytes32 proposalId) external view returns (bool);
 }
